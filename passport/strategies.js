c60e3fd79118349312aeef9c7dcdb59a94777427
Nguyen Huu Tu <tunhbd1998@gmail.com>
2019-10-29 08:26:24 +0700

    update strategy

--- passport/strategies.js
+++ passport/strategies.js
@@ -42,10 +42,6 @@ const useJwtStrategy = () => {
         }
 
         return cb(null, null);
-      })
-      .catch(err => {
-        console.log('err', err);
-        cb(new CustomError(500, err), null)
       });
   }));
 };
